4584e390508a7c5f7ea9024d3ca7c37feaaeff15
thangem1308 <37498415+thangem1308@users.noreply.github.com>
2019-11-20 14:14:34 +0700

    Update productList.php

--- productList.php
+++ productList.php
@@ -21,7 +21,7 @@
                               <li class="list-group-item" style="padding: 0px;">
                                  <div class="checkbox">
                                     <label style="font-size: 1.2em">
-                                    <input type="checkbox" value="" checked>
+                                    <input type="checkbox" value="" unchecked>
                                     <span class="cr"><i class="cr-icon fa fa-check"></i></span> NIKE
                                     </label>
                                  </div>
@@ -89,7 +89,7 @@
                           <li class="list-group-item" style="padding: 0px;">
                              <div class="checkbox">
                                 <label style="font-size: 1.2em">
-                                <input type="checkbox" value="" checked>
+                                <input type="checkbox" value="" unchecked>
                                 <span class="cr"><i class="cr-icon fa fa-check"></i></span> M
                                 </label>
                              </div>
@@ -148,7 +148,7 @@
                           <li class="list-group-item" style="padding: 0px;">
                              <div class="checkbox">
                                 <label style="font-size: 1.2em">
-                                <input type="checkbox" value="" checked>
+                                <input type="checkbox" value="" unchecked>
                                 <span class="cr"><i class="cr-icon fa fa-check"></i></span> Brown
                                 </label>
                              </div>
@@ -183,17 +183,27 @@
                  <!--lọc theo màu sắc-->
               </div>
               <!--phần chính - danh sách các sản phẩm -->
+
+
               <div class="list-product col-md-9 col-xs-9">
                     <div class="col-xs-12" style="padding:0">
                             <p style="background-color: #121212;width: 100%;height: 40px;line-height: 40px;font-weight: bold;padding-left: 10px; color: white;margin-bottom: 15px;">ALL</p>
                     </div>
                     <div class="row">
+                        <?php
+                           $db_connection = pg_connect("host=localhost dbname=footgear user=postgres password=thangem9x");
+                           $sql = "SELECT * FROM product; ";
+                           $result = pg_query($sql);
+
+                           while ($row = pg_fetch_array($result)) {
+                        ?> 
+
                             <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
                                     <div class="sanpham" id="sanpham1">
                                         <!--phần đầu chứa hình ảnh của sản phẩm-->
                                         <div class="header-image-product" style="height: 200px">
                                             <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/sanpham3.jpg" class="img-responsive img-change">
+                                                <img src=" <?php echo $row['productimage']?> " class="img-responsive img-change">
                                             </div>
                                         </div>
                                            <!--kết thúc phần đầu của sản phẩm-->
@@ -201,8 +211,8 @@
                                         <br>
                                         <br>
                                         <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Air Force Ultraforce</p>
-                                            <p class="price-product" style="font-weight: bold">$119</p>
+                                        <br><p style="font-weight: bold"><?php echo $row['productname'] ?></p>
+                                            <p class="price-product" style="font-weight: bold"> <?php echo $row['price']?> </p>
                                               
                                         <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
                                             <p class="footer-product">
@@ -214,145 +224,15 @@
                                            <!--kết thúc phần thông tin của sản phầm-->
                                     </div>
                             </div>
-                            <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
-                                    <div class="sanpham" id="sanpham1">
-                                        <!--phần đầu chứa hình ảnh của sản phẩm-->
-                                        <div class="header-image-product" style="height: 200px">
-                                            <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/nike2.jpg" class="img-responsive img-change">
-                                            </div>
-                                        </div>
-                                           <!--kết thúc phần đầu của sản phẩm-->
-                                           <!--phần thông tin của sản phẩm-->
-                                        <br>
-                                        <br>
-                                        <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Air Jorrdan 12</p>
-                                        <p class="price-product" style="font-weight: bold">$149</p>
-                                              
-                                        <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
-                                            <p class="footer-product">
-                                                <button type="button" class="btn btn-success btn-click-muahang">Shop Now</button>
-                                                <button type="button" class="btn btn-info btn-click-xem-chi-tiet">View Product
-                                                </button>
-                                            </p>
-                                        </div>
-                                           <!--kết thúc phần thông tin của sản phầm-->
-                                    </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
-                                    <div class="sanpham" id="sanpham1">
-                                        <!--phần đầu chứa hình ảnh của sản phẩm-->
-                                        <div class="header-image-product" style="height: 200px">
-                                            <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/nike3.jpg" class="img-responsive img-change">
-                                            </div>
-                                        </div>
-                                           <!--kết thúc phần đầu của sản phẩm-->
-                                           <!--phần thông tin của sản phẩm-->
-                                        <br>
-                                        <br>
-                                        <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Joyride Run Flyknit</p>
-                                        <p class="price-product" style="font-weight: bold">$199</p>
-                                              
-                                        <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
-                                            <p class="footer-product">
-                                                <button type="button" class="btn btn-success btn-click-muahang">Shop Now</button>
-                                                <button type="button" class="btn btn-info btn-click-xem-chi-tiet">View Product
-                                                </button>
-                                            </p>
-                                        </div>
-                                           <!--kết thúc phần thông tin của sản phầm-->
-                                    </div>
-                            </div>
-                    </div>
-                    <div class="row">
-                            <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
-                                    <div class="sanpham" id="sanpham1">
-                                        <!--phần đầu chứa hình ảnh của sản phẩm-->
-                                        <div class="header-image-product" style="height: 200px">
-                                            <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/nike4.jpg" class="img-responsive img-change">
-                                            </div>
-                                        </div>
-                                           <!--kết thúc phần đầu của sản phẩm-->
-                                           <!--phần thông tin của sản phẩm-->
-                                        <br>
-                                        <br>
-                                        <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Air Max</p>
-                                        <p class="price-product" style="font-weight: bold">$99</p>
-                                              
-                                        <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
-                                            <p class="footer-product">
-                                                <button type="button" class="btn btn-success btn-click-muahang">Shop Now</button>
-                                                <button type="button" class="btn btn-info btn-click-xem-chi-tiet">View Product
-                                                </button>
-                                            </p>
-                                        </div>
-                                           <!--kết thúc phần thông tin của sản phầm-->
-                                    </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
-                                    <div class="sanpham" id="sanpham1">
-                                        <!--phần đầu chứa hình ảnh của sản phẩm-->
-                                        <div class="header-image-product" style="height: 200px">
-                                            <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/nike5.jpg" class="img-responsive img-change">
-                                            </div>
-                                        </div>
-                                           <!--kết thúc phần đầu của sản phẩm-->
-                                           <!--phần thông tin của sản phẩm-->
-                                        <br>
-                                        <br>
-                                        <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Everyday Cushioned</p>
-                                        <p class="price-product" style="font-weight: bold">$222</p>
-                                              
-                                        <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
-                                            <p class="footer-product">
-                                                <button type="button" class="btn btn-success btn-click-muahang">Shop Now</button>
-                                                <button type="button" class="btn btn-info btn-click-xem-chi-tiet">View Product
-                                                </button>
-                                            </p>
-                                        </div>
-                                           <!--kết thúc phần thông tin của sản phầm-->
-                                    </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-4 product" style="padding:0;height: 385px" id="pro1">
-                                    <div class="sanpham" id="sanpham1">
-                                        <!--phần đầu chứa hình ảnh của sản phẩm-->
-                                        <div class="header-image-product" style="height: 200px">
-                                            <div class="right" style="width: 80%;;float:left">
-                                                <img src="img/nike7.jpg" class="img-responsive img-change">
-                                            </div>
-                                        </div>
-                                           <!--kết thúc phần đầu của sản phẩm-->
-                                           <!--phần thông tin của sản phẩm-->
-                                        <br>
-                                        <br>
-                                        <div class="info-product" align="center">
-                                        <br><p style="font-weight: bold">Nike Old Skool Platform</p>
-                                        <p class="price-product" style="font-weight: bold">$999</p>
-                                              
-                                        <!--các nút yêu cầu mua hàng và View Product sản phẩm-->
-                                            <p class="footer-product">
-                                                <button type="button" class="btn btn-success btn-click-muahang">Shop Now</button>
-                                                <button type="button" class="btn btn-info btn-click-xem-chi-tiet">View Product
-                                                </button>
-                                            </p>
-                                        </div>
-                                           <!--kết thúc phần thông tin của sản phầm-->
-                                    </div>
-                            </div>
-                    </div>
-                    
-                    
-                        
+                        <?php } ?>  
+                    </div>         
               </div>
            </div>
         </div>
+
+
+
+
         <!--kết thúc phần danh sách các sản phầm-->
      </div>
      </div>
@@ -363,31 +243,31 @@
     <!-- Đây là phần footer -->
 
 
-    <script>
-        // xư lý sự kiện của danh sách chi tiết sản phầm
-$(document).ready(function () {
-    //  xử lý sự kiện khi người dùng click vao sản phẩm
-    $(document).on('click','.product',function () {
+   <script>
+      // xư lý sự kiện của danh sách chi tiết sản phầm
+      $(document).ready(function () {
+      //  xử lý sự kiện khi người dùng click vao sản phẩm
+      $(document).on('click','.product',function () {
+         window.location="productDetails.php";
+      });
+      $(document).on('click','.btn-click-xem-chi-tiet',function () {
         window.location="productDetails.php";
-    });
-    $(document).on('click','.btn-click-xem-chi-tiet',function () {
-        window.location="productDetails.php";
-    });
-    $(document).on('click','.btn-click-muahang',function () {
-        window.location="cartPage.php";
-    });
+      });
+      $(document).on('click','.btn-click-muahang',function () {
+         window.location="cartPage.php";
+      });
 
     
-    // ban đâu sẽ ẩn  hết các thành phần( phần ảnh review và  các nút bấm)
-    $(".product .header-image-product .left").hide();
-    $('.product .info-product .footer-product').hide();
+      // ban đâu sẽ ẩn  hết các thành phần( phần ảnh review và  các nút bấm)
+      $(".product .header-image-product .left").hide();
+      $('.product .info-product .footer-product').hide();
 
-    $('.product').mouseenter(function () {
-        $(this).css('border','1px solid gray').css('border-radius','5px');
-        $(this).find('.header-image-product .left').show();
-        $(this).find('.info-product .footer-product').show();
+      $('.product').mouseenter(function () {
+         $(this).css('border','1px solid gray').css('border-radius','5px');
+         $(this).find('.header-image-product .left').show();
+         $(this).find('.info-product .footer-product').show();
 
-// xử lý sự kiện di chuyển vào các ô nhỏ
+      // xử lý sự kiện di chuyển vào các ô nhỏ
 
         $(this).find('.box').mouseenter(function () {
             $(this).css('border','1px solid');
